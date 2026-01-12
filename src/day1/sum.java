aab63b85d9aa07e1fe55043a259e3f0b1aa36b79
abhijeetbaraskar24 <abhijeetbaraskar2431@gmail.com>
2026-01-13 03:00:05 +0530

    SUM OF A and B

---
+++ src/day1/sum.java
@@ -0,0 +1,18 @@
+package day1;
+
+import java.util.*;
+public class sum
+{
+    public static void main (String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of a : ");
+        int a= sc.nextInt();
+
+        System.out.println("enter the value of b : ");
+        int b=sc.nextInt();
+
+        int sum= a+b;
+        System.out.println(" Addition is : " + sum);
+    }
+}
